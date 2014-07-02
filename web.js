7e4e490a86aee21d37ae448636f1ee1b4f0678c6
BogdanCS <bogdan.stoian11@gmail.com>
2014-07-02 16:50:28 +0100

    Edited

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require ('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) 
-       {response.send('Hello World! 2');});
+
+app.get('/', function(request, response)
+       {response.send(fs.readFileSync("index.html",'utf-8'));});
 
 var port =process.env.PORT || 8080;
 app.listen(port, function()
-          {console.log("Listening on " + port);});
+	  {console.log("Listening on " + port);});
